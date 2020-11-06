b3f9298a78ed191ff3df04850e5b8d321fa06576
abhishekk40 <66176753+abhishekk40@users.noreply.github.com>
2020-11-06 18:47:26 +0530

    Create calculator1.py

---
+++ calculator1.py
@@ -0,0 +1,13 @@
+def add(first_term, second_term):
+    return first_term + second_term
+
+ 
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+print(add(1,2))
+
+
+print(subtract(4,2))
