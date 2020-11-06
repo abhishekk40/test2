38f2a1dc49a77866872fe82734754c932254a919
abhishekk40 <66176753+abhishekk40@users.noreply.github.com>
2020-11-06 18:23:21 +0530

    Update calculator1.py

--- playbooks/calculator1.py
+++ playbooks/calculator1.py
@@ -6,8 +6,7 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     return first_term - second_term
 
-print("calling addition...")
-print("result of addition 1 and 2  is =")
+
 print(add(1,2))
 
 print("\n calling substract....")
