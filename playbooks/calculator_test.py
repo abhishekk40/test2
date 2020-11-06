347bbe547ec73ee2315cf363bb8ee8141f27d269
abhishekk40 <66176753+abhishekk40@users.noreply.github.com>
2020-11-06 16:53:39 +0530

    Update calculator_test.py

--- playbooks/calculator_test.py
+++ playbooks/calculator_test.py
@@ -11,6 +11,7 @@ class TestCalculator(unittest.TestCase):
     def test_addition(self):
         result = calculator.add(1, 2)
         self.assertEqual(result, 3)
+        print("addition assertion completed")
         
 
  
@@ -18,6 +19,7 @@ class TestCalculator(unittest.TestCase):
     def test_subtraction(self):
         result = calculator.subtract(4,2)
         self.assertEqual(result, 2)
+        print("substraction assertion completed")
         
         
 if __name__ == '__main__':
